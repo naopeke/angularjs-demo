4b6d40a46bc5dcb1c7feaa0dc1784986980bb171
naopeke <naopeke@protonmail.com>
2024-07-09 13:29:43 +0200

    filters

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var app = angular.module('myApp', []);
 
 app.controller('MyController', ['$scope', function($scope){
-    $scope.firstname = 'Nao';
-    $scope.lastname = 'Mukai';
-    $scope.fullname = function(){
-        return $scope.firstname + " " + $scope.lastname
-    };
-    $scope.isSpy = true;
-    $scope.codename = 'Naopeke'
+    $scope.employees = [
+        {
+            fname: 'Nao',
+            lname: 'Mukai',
+            salary: 20000
+        },
+        {
+            fname: 'John',
+            lname: 'Doe',
+            salary: 50000
+        },
+        {
+            fname: 'Jane',
+            lname: 'Doen',
+            salary: 40000
+        },
+    ];
 }])
