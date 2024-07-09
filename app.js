55cf20294181965b1a56db353a191264a246cece
naopeke <naopeke@protonmail.com>
2024-07-09 16:35:50 +0200

    submit form

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var app = angular.module('myApp', []);
 
 app.controller('MyController', ['$scope', function($scope){
+    $scope.form = {};
+
+    $scope.addReview = function(){
+        $scope.reviews.push($scope.form);
+        $scope.form ={};
+    }
+
    $scope.reviews = [
     {
         comment: 'Awsome',
-        by: 'Nao'
+        by: 'Nao@example.com'
     },
     {
         comment: 'Fantastic',
-        by: 'Jane'
+        by: 'Jane@example.com'
     },
     {
         comment: 'Excelent',
-        by: 'John'
+        by: 'John@example.com'
     },
    ]
 }])
